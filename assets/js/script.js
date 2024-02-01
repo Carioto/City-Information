fdf995bcdfd1c6b319dc2be8c96e33104cc5d70a
DaveC <dcarioto114@outlook.com>
2024-01-31 21:31:34 -0500

    add aqindex

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
 var mapClick = document.querySelector("#map");
-
-
+var airQEnd = "https://api.waqi.info/feed/"
+var airQApi = "/?token=30986c5df2d22681e0e4e2f639c484959fea837c"
 function getCity(event) {
   var cityClick =  event.target.getAttribute('title');
   
   console.log(cityClick);
+  var airQInd = airQEnd + cityClick + airQApi;
+  console.log(airQInd);
+  fetch(airQInd)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    var airQdata = data;
+    console.log(airQdata);
+    var aqiEl = airQdata.data.aqi;
+    console.log (aqiEl);
+  }
+  )
 }
 
 
